57a5a20a56d8b8b62d56304c8864e4e83e515960
Rico Olivetti <henrique@oerlabs.com.br>
2023-02-23 17:38:58 -0300

    Commit Bot Discord criado em Javascript OER Labs 23/02/2023

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Require the necessary discord.js classes _ 
+// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
 // dotenv
