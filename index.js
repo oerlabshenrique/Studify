d0c8e14b5a1181fbda2f914450d1978e269f7afa
Rico Olivetti <henrique@oerlabs.com.br>
2023-02-24 16:38:31 -0300

    Commit Studify 24/02/2023

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-// Require the necessary discord.js classes alteaça Altera
+// Require the necessary discord.js classes alteaça
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
 // dotenv
 const dotenv = require('dotenv')
 dotenv.config()
-const { TOKEN } = process.env
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
 
 // impoortaçao dos comandos
 const fs = require("node:fs")
 const path = require("node:path");
-const { data, execute } = require('./commands/ping');
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles =fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
@@ -25,12 +24,10 @@ for (const file of commandFiles) {
         client.commands.set(command.data.name, command)
 
     } else {
-        console.log('Esse comando')
+        console.log(`Esse comando em ${filePath} está com "data" ou "execute ausentes"`)
     }
 
 }
-
-console.log(client.commands)
 
 // Login do Bot
 client.once(Events.ClientReady, c => {
@@ -40,6 +37,22 @@ client.login(TOKEN);
 
 // Listener de interações com o bot
 client.on(Events.InteractionCreate, async interaction =>{
+
+
+if (interaction.isStringSelectMenu()){
+        const selected = interaction.values[0]
+        if (selected == "javascript"){
+            await interaction.reply("Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript")
+        } else if (selected == "python"){
+            await interaction.reply("Documentação do Python: https://www.python.org")
+        } else if (selected == "csharp"){
+            await interaction.reply("Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/")
+        } else if (selected == "discordjs"){
+            await interaction.reply("Documentação do Discord.js: https://discordjs.guide/#before-you-begin")
+        }
+    }
+
+
     if (!interaction.isChatInputCommand()) return
     const command = interaction.client.commands.get(interaction.commandName)
     if (!command) {
